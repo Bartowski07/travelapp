558172ae06a1d34a9997298f0fafdca699a87026
MohammadToumouk <129755740+MohammadToumouk@users.noreply.github.com>
2023-06-12 15:45:02 +0200

    update

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Home from "./pages/Home";
 import Hero from "./components/Hero";
+import Navbar from "./components/navbar/Navbar";
+import Aboutus from "./components/aboutus/Aboutus";
 
 export default function App() {
   return (
     <div className="App">
+      <Navbar />
       <Hero />
+      <Aboutus />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
